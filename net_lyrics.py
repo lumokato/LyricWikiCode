20ffd9052ebd9d689a2758412bd7d6d379d642c5
suzto <suzto@qq.com>
2018-03-06 11:22:27 +0800

    去除首末的sep与其他非歌词信息

--- net_lyrics.py
+++ net_lyrics.py
@@ -8,21 +8,27 @@ import time
 import types
 import urllib.request
 
+
 # 一些常用函数
+
+
 def savedoc(filepath, content):
     f = open(filepath, 'w')
     f.write(content)
     f.close()
+
 
 def appenddoc(filepath, content):
     f = open(filepath, 'a')
     f.write(content)
     f.close()
 
+
 def getRegex(regex, data):
     dat = re.compile(regex)
     ret = re.findall(dat, data)
     return ret
+
 
 def getCited(sen):
     reg = r'"([^\"]+)\"'
@@ -30,21 +36,24 @@ def getCited(sen):
     kword = re.findall(kw, sen)
     if kword:
         return kword
-# 去除文件名显示错误
+
+
 # def validateTitle(title):
+#     去除文件名显示错误
 #     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/\:*?"<>|'
 #     new_title = re.sub(rstr, "__", title)
 #     return new_title
 
-# 读取指定路径的文件
+
 # def getdoclist(docpath):
+#     读取指定路径的文件
 #     docdata = open(docpath)
 #     doclist = docdata.read().splitlines()
 #     return doclist
 
 
-# 获取网页信息
 def getHtml(url):
+    # 获取网页信息
     hds = {'Cookie': 'os=pc; osver=Microsoft-Windows-8-Professional-build-9200-64bit; appver=1.5.0.75771;',
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.138 Safari/537.36',
            'Referer': 'http://music.163.com/'}
@@ -54,8 +63,8 @@ def getHtml(url):
     return html
 
 
-# 根据社团id获取专辑列表
 def getAlbumList(id_Group):
+    # 根据社团id获取专辑列表
     url_Group = 'http://music.163.com/api/artist/albums/' + str(id_Group) + '?id=' + str(
         id_Group) + '&offset=0&total=true&limit=1000'
     page_Group = getHtml(url_Group)
@@ -71,29 +80,38 @@ def getAlbumList(id_Group):
     return abmlist
 
 
-# 根据专辑id获取歌词页面
-def getTrackList(id_Album,name_Album):
-    url_Album = 'http://music.163.com/api/album/' + str(id_Album) + '?ext=true&id=' + str(
-        id_Album) + '&offset=0&total=true&limit=1000'
-    page_album = getHtml(url_Album)
+def getTrackList(id_album, name_album):
+    # 根据专辑id获取歌词页面
+    url_album = 'http://music.163.com/api/album/' + str(id_album) + '?ext=true&id=' + str(
+        id_album) + '&offset=0&total=true&limit=1000'
+    page_album = getHtml(url_album)
     page_rev = page_album.decode()[::-1]
     reg1 = r'"R_SO_4_(\d+)"'
     songid_total = getRegex(reg1, page_album.decode())
     song_list = {}
     for songid in songid_total:
         song_name = getRegex(songid[::-1] + r':"di","(.+?)"', page_rev)[0][::-1]
-        song_list[songid] = song_name + "    " + name_Album
+        song_list[songid] = song_name + "    " + name_album  # 将曲名与专辑名合并为一个变量
     return song_list
 
-# 获取歌词页面
+
 def songpage(sid, sname, savetxt):
-    writelinez = ['\n\n#####################################\n\n', sname,"    "]
-    url_Track = 'http://music.163.com/api/song/lyric?os=pc&id=' + str(sid) + '&lv=-1&kv=-1&tv=-1'
-    page_song = getHtml(url_Track)
+    # 获取歌词页面
+    writelinez = ['\n\n#####################################\n\n', sname, "    "]
+    url_track = 'http://music.163.com/api/song/lyric?os=pc&id=' + str(sid) + '&lv=-1&kv=-1&tv=-1'
+    page_song = getHtml(url_track)
     if len(page_song) > 150:
         try:
             print(sname)
-            writelinez.extend(lrctotxt(page_song.decode()))
+            writeline_all = lrctotxt(page_song.decode())
+            writelinez.extend(writeline_all[0])
+            if writeline_all[1]:
+                writeline_re = ['\n\n#####################################\n\n', sname,
+                                '\n\n#####################################\n\n']
+                writeline_re.extend(writeline_all[1])
+                with open(savetxt + '_re', 'a', encoding='utf-8') as file:
+                    for writere in writeline_re:
+                        file.write('%s' % (writere))
             with open(savetxt, 'a', encoding='utf-8') as file:
                 for writex in writelinez:
                     file.write('%s' % (writex))
@@ -101,8 +119,20 @@ def songpage(sid, sname, savetxt):
             appenddoc('rubbish', str(sid) + '\n')
 
 
-# 根据歌词页面转化为wiki格式
+def lyric_define(line_ja):
+    # 判断歌词是否为附加信息
+    info_re = ["作曲", "作词", "原曲",
+               "サークル：", "アルバム：",
+               "Album", "Circle", "Origin",
+               "Vocal", "Lyric", "Arrange"]
+    for keywords in info_re:
+        if keywords in line_ja:
+            return False
+    return True
+
+
 def lrctotxt(songpage):
+    # 根据歌词页面转化为wiki格式
     writeline = []
     lrc_det = getRegex(r'"lyricUser":{.+?}', songpage)
     if lrc_det:
@@ -112,6 +142,8 @@ def lrctotxt(songpage):
     writeline.append('__LYRICS__\n\n{{歌词信息|\n')
     lrc = getRegex(r'"lrc":{(.*?}),"', songpage)[0]
     tlrc = getRegex(r'"tlyric":{(.*?}),"', songpage)[0]
+    writeline_l = []  # 保留的部分
+    writeline_d = []  # 去除的部分
     # 在有翻译时
     if tlrc != '"version":0,"lyric":null}':
         if tlrc[-4:] != '\\n"}':
@@ -129,35 +161,40 @@ def lrctotxt(songpage):
             for lines in linex:
                 linen = lines
                 timenx = getRegex(r'\[(\d.+?)\]', lines)
-                sen = getRegex(r'(.*?)\]\d', linen[::-1])[0][::-1]
+                sen = getRegex(r'(.*?)\]\d', linen[::-1])[0][::-1].strip()
                 if not sen:
                     sen = ''
                 for timen in timenx:
                     timeline[timen] = [sen, '']
             for tlines in tlinex:
                 ttimenx = getRegex(r'\[(\d.+?)\]', tlines)
-                tsen = getRegex(r'(.*?)\]\d', tlines[::-1])[0][::-1]
+                tsen = getRegex(r'(.*?)\]\d', tlines[::-1])[0][::-1].strip()
                 for ttimen in ttimenx:
                     if ttimen in timeline.keys():
                         senj = timeline[ttimen][0]
                         timeline[ttimen] = [senj, tsen]
                     else:
-                        timeline[ttimen] = ''
+                        timeline[ttimen] = ['', tsen]
             for tls in sorted(timeline.keys()):
                 if timeline[tls] == '':
-                    writeline.append('sep=' + tls + '\n\n')
+                    writeline_l.append('sep=' + tls + '\n\n')
                 else:
                     if type(timeline[tls][0]) == list:
                         appenddoc('rubbish', 'xx' + '\n')
                         break
                     else:
-                        if timeline[tls][0].strip() == '':
-                            writeline.append('sep=' + tls + '\n\n')
+                        if timeline[tls][0] == '' and timeline[tls][1] == '':
+                            writeline_l.append('sep=' + tls + '\n\n')
                         else:
-                            writeline.append('time=' + tls + '\n')
-                            writeline.append('ja=' + timeline[tls][0] + '\n')
-                            writeline.append('zh=' + timeline[tls][1] + '\n\n')
-                            # 如果无时间轴
+                            if lyric_define(timeline[tls][0]):
+                                writeline_l.append('time=' + tls + '\n')
+                                writeline_l.append('ja=' + timeline[tls][0] + '\n')
+                                writeline_l.append('zh=' + timeline[tls][1] + '\n\n')
+                            else:
+                                writeline_d.append('time=' + tls + '\n')
+                                writeline_d.append('ja=' + timeline[tls][0] + '\n')
+                                writeline_d.append('zh=' + timeline[tls][1] + '\n\n')
+        # 如果无时间轴
         else:
             wordline = getRegex(r'"lyric":"(.*?)"}', lrc)[0]
             tranline = getRegex(r'"lyric":"(.*?)"}', tlrc)[0]
@@ -168,11 +205,16 @@ def lrctotxt(songpage):
             else:
                 for i in range(len(wordn)):
                     if wordn[i].strip() == '':
-                        writeline.append('sep=\n\n')
+                        writeline_l.append('sep=\n\n')
                     else:
-                        writeline.append('time=\n')
-                        writeline.append('ja=' + wordn[i] + '\n')
-                        writeline.append('zh=' + trann[i] + '\n\n')
+                        if lyric_define(wordn[i]):
+                            writeline_l.append('time=\n')
+                            writeline_l.append('ja=' + wordn[i] + '\n')
+                            writeline_l.append('zh=' + trann[i] + '\n\n')
+                        else:
+                            writeline_d.append('time=\n')
+                            writeline_d.append('ja=' + wordn[i] + '\n')
+                            writeline_d.append('zh=' + trann[i] + '\n\n')
     else:
         writeline.append('| 语言 = 日文\n| 翻译 =\n| 译者 =\n}}\n\nlyrics=\n\n')
         linex = getRegex(r'(\[\d.*?)\\n', lrc)
@@ -183,14 +225,19 @@ def lrctotxt(songpage):
                 timenx = getRegex(r'\[(.*?)\]', lines)
                 sen = getRegex(r'(.*?)\]', linen[::-1])[0][::-1]
                 for timen in timenx:
-                    timeline[timen.lstrip()] = sen
+                    timeline[timen.lstrip()] = sen.strip()
             for tls in sorted(timeline.keys()):
-                if timeline[tls].strip() == '':
-                    writeline.append('sep=' + tls + '\n\n')
+                if timeline[tls] == '':
+                    writeline_l.append('sep=' + tls + '\n\n')
                 else:
-                    writeline.append('time=' + tls + '\n')
-                    writeline.append('ja=' + timeline[tls] + '\n')
-                    writeline.append('zh=\n\n')
+                    if lyric_define(timeline[tls]):
+                        writeline_l.append('time=' + tls + '\n')
+                        writeline_l.append('ja=' + timeline[tls] + '\n')
+                        writeline_l.append('zh=\n\n')
+                    else:
+                        writeline_d.append('time=' + tls + '\n')
+                        writeline_d.append('ja=' + timeline[tls] + '\n')
+                        writeline_d.append('zh=\n\n')
         else:
             wordline = getRegex(r'"lyric":"(.*?)"}', lrc)[0]
             wordn = getRegex(r'(.*?)\\n', wordline)
@@ -201,14 +248,32 @@ def lrctotxt(songpage):
                 if not wordnx[i].strip():
                     writeline.append('sep=\n\n')
                 else:
-                    writeline.append('time=\n')
-                    writeline.append('ja=' + wordnx[i] + '\n')
-                    writeline.append('zh=\n\n')
-    return writeline
+                    if lyric_define(wordnx[i]):
+                        writeline_l.append('time=\n')
+                        writeline_l.append('ja=' + wordnx[i] + '\n')
+                        writeline_l.append('zh=\n\n')
+                    else:
+                        writeline_d.append('time=\n')
+                        writeline_d.append('ja=' + wordnx[i] + '\n')
+                        writeline_d.append('zh=\n\n')
+    linenum = 0
+    for line_for in range(0, writeline_l.__len__()):
+        if "sep=" in writeline_l[line_for]:
+            linenum += 1
+        else:
+            break
+    writeline_lx = writeline_l[linenum:]
+    for line_rev in range(0, writeline_l.__len__())[::-1]:
+        if "sep=" in writeline_l[line_rev]:
+            writeline_lx.pop()
+        else:
+            break
+    writeline.extend(writeline_lx)
+    return [writeline, writeline_d]
 
 
-# 进行文本输出
 def saveAll(groupnum):
+    # 进行文本输出
     group = open('GroupId', 'rb').readlines()[groupnum]
     groupname = getCited(group.decode())[0]
     writelinex = [groupname, '\n**************************\n\n']
@@ -232,6 +297,6 @@ def saveAll(groupnum):
 
 
 # print(getAlbumList(19783)[1][0])
-saveAll(49)
 # getTrackList(82840,"test")
 # songpage('27580735','xx','xxx')
+saveAll(18)
